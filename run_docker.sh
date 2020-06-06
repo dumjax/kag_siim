7514053b51fc7711b004c9d068a7597dbf66e1bd
Julien Herzen <j.herzen@gmail.com>
2020-06-06 20:22:09 +0200

    improvements

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-sudo nvidia-docker run -it -p 8888:8888 \
+sudo nvidia-docker run -it -p 8888:8888 --shm-size 8G \
   -v `pwd`/data:/workdir/data \
   -v `pwd`/src:/workdir/src \
   -v `pwd`/notebooks:/workdir/notebooks \
