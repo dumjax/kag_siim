706ec9b129f85e56391cf1900db6be4766a62957
Julien Herzen <julien@unit8.co>
2020-06-18 17:10:15 +0200

    update experiment file

--- src/julien_run_1.py
+++ src/julien_run_1.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 import albumentations
 
+from copy import copy
 from engine import launch
 from architectures import TimmModel
 
@@ -15,39 +16,39 @@ STD = (0.229, 0.224, 0.225)
 """
 TODO
 
-* Reduce overfitting (more augmentations, more randomness, Dropout?)
-* Exponential/linear decrease of LR
+* Check image scaling method depending on model (bicubic vs bilinear)
 """
 
 config = {
     ### Global parameters
-    'NAME': 'julien_014',
+    'NAME': 'julien_029',
     'SCRIPT_NAME': os.path.basename(__file__),
     'SEED': 41,
     'DEVICE': 'cuda',
     'FOLDS_FILENAME': 'train_folds_5.csv',
-    'NR_FOLDS': 2,  # Number of folds to complete
-    'TRAINING_DATA_PATH': '../data/input/train224/',
+    'NR_FOLDS': 3,  # Number of folds to complete
+    'TRAINING_DATA_PATH': '../data/input/train380/',
     
     ### Model parameters
     'MODEL_CLS': TimmModel,
-    'PRETRAINED_MODEL': 'mixnet_xl',  # Don't forget to update the training data path with correct resolution
+    'PRETRAINED_MODEL': 'tf_efficientnet_b4_ns',  # Don't forget to update the training data path with correct resolution
     'FINETUNING': True,
-    'USE_GENDER': True,
+    'USE_GENDER': False,
     'USE_AGE': True,
-    'USE_SITES': True,
+    'USE_SITES': False,
     'HIDDEN_SIZES': [],
     'NONLINEARITY': F.relu,
 
     ### Training parameters:
     'NR_EPOCHS': 50,
-    'TRAIN_BATCHSIZE': 32,
-    'VALID_BATCHSIZE': 32,
+    'TRAIN_BATCHSIZE': 8,
+    'VALID_BATCHSIZE': 16,
 
     'OPTIMIZER_CLS': torch.optim.Adam,
     'OPTIMIZER_KWARGS': {'lr': 5e-5},  # all arguments passed to optimizer, except model parameters
-    'SCHEDULER_CLS': torch.optim.lr_scheduler.ReduceLROnPlateau,
-    'SCHEDULER_KWARGS': {'patience':3, 'threshold':0.001, 'mode':'max'},  # all arguments except optimizer
+    # 'SCHEDULER_CLS': torch.optim.lr_scheduler.ExponentialLR,
+    'SCHEDULER_CLS': None,
+    'SCHEDULER_KWARGS': {'gamma': 0.9},  # all arguments except optimizer
     'APPLY_SCHEDULER_EACH_MINIBATCH': False,  # If False, apply each epoch. No scheduler if SCHEDULER_CLS is None
 
     'EARLYSTOP_PATIENCE': 7,
@@ -57,10 +58,12 @@ config = {
     'TRAIN_AUGMENTATIONS': albumentations.Compose(
             [
                 albumentations.Normalize(MEAN, STD, max_pixel_value=255.0, always_apply=True),
-                albumentations.RGBShift(p=0.2),
-                albumentations.RandomContrast(p=0.2),
-                albumentations.GridDropout(ratio=0.1, p=0.2),
-                albumentations.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.5),
+                # albumentations.RGBShift(p=0.2),
+                # albumentations.RandomContrast(p=0.2),
+                # albumentations.GridDropout(ratio=0.1, p=0.2),
+                # albumentations.RandomBrightnessContrast(p=0.5),
+                # albumentations.RandomContrast(p=0.5),
+                albumentations.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=90, p=0.8),
                 albumentations.Flip(p=0.5)
             ]),
     'VALID_AUGMENTATIONS': albumentations.Compose(
@@ -70,4 +73,5 @@ config = {
 }
 
 if __name__ == '__main__':
-    launch(config)
+    res1 = launch(config)
+    print('res1: {}'.format(res1))
